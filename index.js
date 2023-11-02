db0e26337a59883fb10b8926baf85c8d0f2a2864
Alyssa Moore <139104991+zhongjueping@users.noreply.github.com>
2023-11-02 06:48:29 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+_manCard = {
+            asynJs : [],
+            asynLoad : function(id){
+                _manCard.asynJs.push(id);
+            }
+        };
+        window._sp_async = 1;
+        
+        
+        
+            
+        
+        window.bds = {
+            comm: {
+                ishome: 1,
+                qid: "0xb3ceb8da00040ecd",
+                alwaysMonitor: "0"
+            }
+        };
